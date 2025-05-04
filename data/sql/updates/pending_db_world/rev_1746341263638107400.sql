864c2c1b5b076cdb37c6fcbfc93e662338ed470d
avarishd <46330494+avarishd@users.noreply.github.com>
2025-05-04 09:48:05 +0300

    fix(DB/Procs): Spell Fury being consumed on dots

---
+++ data/sql/updates/pending_db_world/rev_1746341263638107400.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM `spell_proc_event` WHERE `entry`=46102;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `procPhase`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES
+(46102, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
